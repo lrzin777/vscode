126e14402e448545b742425adf8ea8ccd5689bbc
lrzin777 <leonardo.helmann@escola.pr.gov.br>
2025-06-25 20:06:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,14 @@
+criaCartao(
+'progamação',
+'O que é Python?',
+'O Python é uma linguagem de progamação')
+
+criaCartao(
+'Geografia',
+'Qual a capital da França?',
+'A capital da França é Paris')
+
+criaCartao(
+'Progamação',
+'O que é uma função?',
+'Uma função é um bloco de código que executa alguma tarefa')
